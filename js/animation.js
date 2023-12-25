0b4aba9ae99bcfa310daab4fdcf7a6c3714dbd24
flaim <21@domain.kf.mrsu.ru>
2023-12-25 13:10:12 +0300

    1.9 v

--- js/animation.js
+++ js/animation.js
@@ -21,10 +21,15 @@ window.addEventListener("scroll", function() {
   }
 });
 
-const a1 = document.querySelector('.a1')
-const a2 = document.querySelector('.a2')
+const element2 = document.querySelector('.a2');
+const element3 = document.querySelector('.a1');
 
-a1.addEventListener('click', function() {
-  a2.classList.remove('a2')
-  a2.classList.add('a3')
+element2.addEventListener('click', () =>{
+  element2.classList.add('a2hover');
+  element3.classList.add('a2')
+})
+
+element3.addEventListener('click', () =>{
+  element3.classList.remove('a2');
+  element2.classList.remove('a2hover')
 })
