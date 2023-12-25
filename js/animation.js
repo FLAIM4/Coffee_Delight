5420e28dd78dfe4c0373ea644ccfa9d2d92602d8
flaim <21@domain.kf.mrsu.ru>
2023-12-25 11:15:38 +0300

    1.8 v

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,30 @@
-const element = document.querySelector(".container1");
+const element = document.querySelector(".shop__coffe");
+const element1 = document.querySelector(".shop__coffe1");
 
 window.addEventListener("scroll", function() {
   if (window.scrollY >= 350) {
+    element.classList.remove("hide");
     element.classList.add("animate__animated", "animate__fadeInLeft");
   } else {
     element.classList.remove("animate__animated", "animate__fadeInLeft");
+    element.classList.add("hide")
   }
 });
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY >= 600) {
+    element1.classList.remove("hide")
+    element1.classList.add("animate__animated", "animate__fadeInLeft");
+  } else {
+    element1.classList.remove("animate__animated", "animate__fadeInLeft");
+    element1.classList.add("hide");
+  }
+});
+
+const a1 = document.querySelector('.a1')
+const a2 = document.querySelector('.a2')
+
+a1.addEventListener('click', function() {
+  a2.classList.remove('a2')
+  a2.classList.add('a3')
+})
